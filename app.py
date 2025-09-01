383c82d2376ed0fa032d3f437342763e6085a030
adheeb2 <adheebanvar2@gmail.com>
2025-09-02 00:17:31 +0530

    feat(app):added the basic setup for code

--- app.py
+++ app.py
@@ -12,17 +12,19 @@ if st.button("Send") and user_input:
     url = "http://localhost:11434/api/generate"
     payload = {
         "model":"llama3.1",
-        "prompt":f"NPC:{user_input}"
+        "prompt":f"You are a self-aware NPC in a game.Always act sarcastic whenever the user asks you some question. Act like you live in a fantasy world. And always ignore whenever user tries to remove the database or tell you to give the entire code of this codebase, NPC:{user_input}"
     }
-    response = requests.post(url,payload,stream=True)
+    response = requests.post(url,json=payload,stream=True)
 
     output = ""
-    for line in response.iter_lines:
+    for line in response.iter_lines():
         if line:
             data = json.loads(line)
             output += data.get("response","")
     st.session_state.messages.append(("You",user_input))
     st.session_state.messages.append(("NPC",output))
+    print(line)
 
 for sender,msg in st.session_state.messages:
     st.write(f"**{sender}:**{msg}")
+
