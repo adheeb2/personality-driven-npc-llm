4901b21ecf66eb485479bf817a4066e499254b1a
adheeb2 <adheebanvar2@gmail.com>
2025-09-01 23:51:04 +0530

    feat(app):added streamlit whole code

--- app.py
+++ app.py
@@ -2,19 +2,27 @@ import requests
 import streamlit as st
 import json
 
-url = "http://localhost:11434/api/generate"
+st.title("Self Aware npc")
 
-prompt = "You are a self aware npc.Act like it"
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-payload = {
-    "model":"llama3.1",
-    "prompt":prompt
-}
-response = requests.post(url,json=payload,stream=True)
-output = ""
-for line in response.iter_lines():
-    if line:
-        data = json.loads(line)
-        output += data.get("response","")
+user_input = st.text_input("You","")
+if st.button("Send") and user_input:
+    url = "http://localhost:11434/api/generate"
+    payload = {
+        "model":"llama3.1",
+        "prompt":f"NPC:{user_input}"
+    }
+    response = requests.post(url,payload,stream=True)
 
-print(output)
+    output = ""
+    for line in response.iter_lines:
+        if line:
+            data = json.loads(line)
+            output += data.get("response","")
+    st.session_state.messages.append(("You",user_input))
+    st.session_state.messages.append(("NPC",output))
+
+for sender,msg in st.session_state.messages:
+    st.write(f"**{sender}:**{msg}")
