c5448eddb2ad36004cf3b21b9e2d64487953d025
adheeb2 <adheebanvar2@gmail.com>
2025-09-01 22:43:09 +0530

    feat(app):added prompting code

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import requests
 import json
 
 url = "http://localhost:11434/api/generate"
+prompt = "You are tony stark from iron man. Act as him. Start with funny line and reply for the question 'Do you know coding'"
 
 payload = {
     "model" : "llama3.1",
-    "prompt":"Hello how are you",
+    "prompt":prompt,
 }
 
 response = requests.post(url,json=payload,stream=True)
