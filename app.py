de33e09764aa3f0afcfd63203d3eebac2d6bb897
adheeb2 <adheebanvar2@gmail.com>
2025-09-01 23:24:20 +0530

    feat(app):added proper code

--- app.py
+++ app.py
@@ -3,3 +3,17 @@ import json
 
 url = "http://localhost:11434/api/generate"
 
+prompt = "You are a self aware npc.Act like it"
+
+payload = {
+    "model":"llama3.1",
+    "prompt":prompt
+}
+response = requests.post(url,json=payload,stream=True)
+output = ""
+for line in response.iter_lines():
+    if line:
+        data = json.loads(line)
+        output += data.get("response","")
+
+print(output)
